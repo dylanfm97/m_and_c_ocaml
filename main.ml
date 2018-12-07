18716b2de89f2678868cc015eb104eb649252559
Dylan Martinez <dylanmartinez@Dylans-MacBook-Air.local>
2018-12-07 11:25:19 -0700

    wow I can actually iterate over a list oh boy

--- main.ml
+++ main.ml
@@ -1,4 +1,9 @@
 
+let print_list state_in = 
+	let print_number num = Printf.printf "%d " num in 
+	List.iter print_number state_in;
+	Printf.printf "\n"
+
 class ['a] state init = object(self)
 	val mutable state : 'a list = init
 
@@ -9,7 +14,6 @@ class ['a] state init = object(self)
 				"true"
 			else
 				"false"
-			
 		| _ -> "false"
 
 	method is_safe =
@@ -19,17 +23,24 @@ class ['a] state init = object(self)
 		| 0 :: _ :: _ -> "true"
 		| _ -> "false"
 
+	method is_goal = 
+		match state with
+		| 0 :: 0 :: _ -> true
+		| _ -> false
+
+
 	method get_state = state
 
-	(* method move move_in = 
-		if move_in = "twomissionaries" then
-			
-		else
-			[] *)
+	method print_state =
+		print_list state
 
 end;;
 
-
+let move_boat boat =
+	if boat = 0 then
+		1
+	else
+		0
 
 let make_move current how_many boat_loc =
 	if boat_loc == 0 then
@@ -40,40 +51,70 @@ let make_move current how_many boat_loc =
 let move move_in state_in = 
 	match state_in#get_state with
 	| miss :: cann :: boat :: tail ->
- 		if move_in == "twomissionaries" then
-			new state([make_move miss 2 boat; cann; boat]) 
-		else if move_in == "onemissionary" then
-			new state([make_move miss 1 boat; cann; boat]) 
-		else if move_in == "oneofeach" then
-			new state([make_move miss 1 boat; make_move cann 1 boat; boat]) 
-		else if move_in == "onecannibal" then
-			new state([miss; make_move cann 1 boat; boat]) 
-		else if move_in == "twocannibals" then
-			new state([miss; make_move cann 2 boat; boat]) 
+ 		if move_in = "twomissionaries" then
+			new state([make_move miss 2 boat; cann; move_boat boat]) 
+		else if move_in = "onemissionary" then
+			new state([make_move miss 1 boat; cann; move_boat boat]) 
+		else if move_in = "oneofeach" then
+			new state([make_move miss 1 boat; make_move cann 1 boat; move_boat boat]) 
+		else if move_in = "onecannibal" then
+			new state([miss; make_move cann 1 boat; move_boat boat]) 
+		else if move_in = "twocannibals" then
+			new state([miss; make_move cann 2 boat; move_boat boat]) 
 		else
-			state_in
-	| _ -> state_in
+			new state([0;0;0])
+	| _ -> new state([0;0;0])
 
+
+
+
+let generate_states state_in = 
+	[move "onemissionary" state_in; move "twomissionaries" state_in; move "oneofeach" state_in; move "onecannibal" state_in; move "twocannibals" state_in] 
+
+
+
+
+(* let rec search =  *)
 
 
 
 let something = new state([3;3;0]) ;;
-let new_state = move "onemissionary" something
-let state_list = new_state#get_state
-for num in state_list do
-	print_int num
-done
 
-(* print_string stuff ;;
-print_string "\n" ;;  *)
- 
+let the_state = something#get_state ;;
 
 
-(* let generate_states state_in = 
-	let valid_states = [];
-	match state_in with
-	| miss :: cann :: boat :: tail ->
-		if boat = 0 then *)
+
+(* let print_it num = print_int num in
+List.iter print_it the_state ;; *)
+
+(* List.iter (fun s -> print_int s) the_state ;; *)
+
+(* let a_list = ["hello"; "world"] in
+List.iter (fun s -> print_endline s) a_list ;; *)
+
+
+
+let new_states = generate_states something ;;
+
+let print_state state_in = state_in#print_state in
+List.iter print_state new_states ;;
+
+
+
+
+(* let s elem = elem in
+List.iter s new_states ;; *)
+
+
+(* let something = new state([3;3;0]) ;;
+let new_state = move "onemissionary" something ;;
+let state_list = new_state#get_state ;;
+
+let other_state = move "twocannibals" new_state ;;
+let this_state_list = other_state#get_state ;;
+ *) 
+
+
 
 
 
